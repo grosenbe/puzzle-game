7dd01e9b17b7ab39e7ca0f926ccffff4c6ef09a0
Geoff Rosenberg <geoff@cgrosenberg.com>
2021-04-03 19:54:20 -0700

    Add testing back in, and build googletest as root.

--- ci_build.sh
+++ ci_build.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
-sudo apt-get install ninja-build
+sudo apt-get install ninja-build googletest
 
 export CC=$(which clang)
 export CXX=$(which clang++)
+
+# install googletest
+sudo mkdir /usr/src/googletest/build
+cd /usr/src/googletest/build
+sudo cmake -G "Ninja" ..
+sudo ninja
+sudo nina install
+cd -
 
 mkdir build
 cd build
 cmake -G "Ninja" ..
 ninja package
 
-# test/puzzle-test
+test/puzzle-test
