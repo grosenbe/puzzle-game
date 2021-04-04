e36f8ae5b0a1583c22276c3851328baeeb82f5f9
Geoff Rosenberg <geoff@cgrosenberg.com>
2021-04-03 18:42:43 -0700

    Switch back to default compiler (g++) and make.

--- ci_build.sh
+++ ci_build.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
-export CC=$(which clang)
-export CXX=$(which clang++)
-
 [ ! -d "build" ] && mkdir build
 rm -rf build/*
 cd build
-cmake -G "Ninja" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=DEBUG ..
-ninja package
+cmake -G "Unix Makefiles" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=DEBUG ..
+make package
 
 test/puzzle-test
